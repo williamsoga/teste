ec85f63998d3df6b6412f00405db37225353f8a3
williamsoga <williamhsoga@gmail.com>
2025-03-25 08:39:54 -0300

    pasta lista1

---
+++ lista/lista/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando uma lista de frutas
+        List<string> frutas = new List<string> { "maçã", "banana", "laranja", "uva" };
+
+        // Adicionando um item à lista
+        frutas.Add("manga");
+
+        // Removendo um item da lista
+        frutas.Remove("banana");
+
+        // Acessando um item da lista pelo índice
+        string primeiraFruta = frutas[0];
+
+        // Imprimindo a lista e o item acessado
+        Console.WriteLine("Lista de frutas: " + string.Join(", ", frutas));
+        Console.WriteLine("Primeira fruta: " + primeiraFruta);
+    }
+}
