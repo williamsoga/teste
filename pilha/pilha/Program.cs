0cf585066fe7990ef8c65be769e7035f7419a0e3
williamsoga <williamhsoga@gmail.com>
2025-03-25 09:06:00 -0300

    adicionar pasta pilha

---
+++ pilha/pilha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> pilha = new Stack<int>();
+
+        // Adicionando elementos à pilha
+        pilha.Push(10);
+        pilha.Push(20);
+        pilha.Push(30);
+
+        // Exibindo o elemento do topo
+        Console.WriteLine("Elemento no topo: " + pilha.Peek());
+
+        // Removendo elementos da pilha
+        Console.WriteLine("Elemento removido: " + pilha.Pop());
+        Console.WriteLine("Elemento removido: " + pilha.Pop());
+
+        // Verificando se a pilha está vazia
+        Console.WriteLine("A pilha está vazia? " + (pilha.Count == 0));
+    }
+}
